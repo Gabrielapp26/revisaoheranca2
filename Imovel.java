4c6c4313eb69f8ddba18ac95550e91f2411a43fe
Gabriela Pellin Perosa <gpp260203@gmail.com>
2022-02-22 18:57:18 +0000

    Criação da classe Imóvel

---
+++ Imovel.java
@@ -0,0 +1,19 @@
+public class Imovel{
+
+    private String endereco;
+    private double preco;
+
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
